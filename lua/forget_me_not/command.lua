b8cabba93620320aef35923a9abe461fb84f2dea
daic0r <daic0r.dev@gmail.com>
2024-01-30 21:57:33 +0100

    feat: implemented Command and tests

--- lua/forget_me_not/command.lua
+++ lua/forget_me_not/command.lua
@@ -4,6 +4,19 @@ local Motion = require("forget_me_not.motion")
 
 local Command = {}
 
+Command.metatable = {
+   __index = Command,
+   __eq = function(a, b)
+      return a.count1 == b.count1 and
+         a.operator == b.operator and
+         a.count2 == b.count2 and
+         a.motion == b.motion
+   end,
+   __tostring = function(self)
+      return tostring(self.count1) .. tostring(self.operator) .. tostring(self.count2) .. tostring(self.motion)
+   end
+}
+
 function Command.new(cnt1, op, cnt2, motion)
    local ret = {
       count1 = cnt1,
@@ -11,9 +24,31 @@ function Command.new(cnt1, op, cnt2, motion)
       count2 = cnt2,
       motion = motion
    }
-   return setmetatable(ret, {
-      __index = Command
-   })
+   return setmetatable(ret, Command.metatable)
+end
+
+function Command:parse(str)
+   local cnt1 = Count.new()
+   str = cnt1:parse(str)
+   print("After Count 1: " .. str .. ", cnt1: " .. tostring(cnt1))
+   local op = Operator.new()
+   str = op:parse(str)
+   if str == nil then
+      return nil
+   end
+   print("After op: " .. str .. ", op: " .. tostring(op))
+   local cnt2 = Count.new()
+   str = cnt2:parse(str)
+   print("After Count 2: " .. str .. ", cnt2: " .. tostring(cnt2))
+   local motion = Motion.new()
+   str = motion:parse(str)
+   if str == nil then
+      return nil
+   end
+   print("After motion: " .. str .. ", motion: " .. tostring(motion))
+   self.count1, self.operator, self.count2, self.motion = cnt1, op, cnt2, motion
+   print("Command: " .. tostring(self))
+   return str
 end
 
 return Command
---
+++ lua/forget_me_not/command_spec.lua
@@ -0,0 +1,21 @@
+describe("Command:", function()
+   it("should be able to parse a command", function()
+      local Command = require("forget_me_not.command")
+      local cmd = Command.new()
+      local str = "3d2w"
+      str = cmd:parse(str)
+      assert.are.equal(str, "")
+      assert.are.equal(cmd.count1.value, 3)
+      assert.are.equal(cmd.operator.operator, "d")
+      assert.are.equal(cmd.count2.value, 2)
+      assert.are.equal(cmd.motion.motion, "w")
+
+      str = "y2f,"
+      str = cmd:parse(str)
+      assert.are.equal(str, "")
+      assert.are.equal(cmd.count1.value, 1)
+      assert.are.equal(cmd.operator.operator, "y")
+      assert.are.equal(cmd.count2.value, 2)
+      assert.are.equal(cmd.motion.motion, "f,")
+   end)
+end)
